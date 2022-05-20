5b97e6e9f95114c3fa5d3c2c5f12a69af0a43c9e
AlinaSyniavska <alina22syniavska@gmail.com>
2022-05-20 18:22:01 +0300

    final

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import {UsersComponent} from "./components/users/users.component";
 import {UserDetailsComponent} from "./components/user-details/user-details.component";
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HttpClientModule} from "@angular/common/http";
 
-import { UserRoutingModule } from './user-routing.module';
+import {UserRoutingModule} from './user-routing.module';
 import {UserService} from "./services";
-import { UserDetailsComponent } from './components/user-details/user-details.component';
-import { UsersComponent } from './components/users/users.component';
-import { UserComponent } from './components/user/user.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
 
 
 @NgModule({
@@ -21,7 +21,7 @@ import { UserComponent } from './components/user/user.component';
     UserRoutingModule
   ],
   providers: [
-    UserService
+    UserService,
   ]
 })
 export class UserModule { }
