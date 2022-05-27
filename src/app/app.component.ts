231b230cb3ac0a61ada9f24ad060dd59ee698950
AlinaSyniavska <alina22syniavska@gmail.com>
2022-05-28 01:22:28 +0300

    add lesson6 emit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {IUser} from "./interfaces";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngproject';
+  user: IUser;
+
+  catchUserEmitToApp($event: IUser) {
+    this.user = $event;
+  }
 }
